75c7d513f6c927d2e23ffc1b735e56eacb7f1445
FluffyTerror <8353207@gmail.com>
2025-03-11 18:38:48 +0300

    Reapply "cucumber start"
    This reverts commit bb54bba075edee00bef31926cd08cb3c7b8b3ce2.

---
+++ src/test/java/org/FluffyTerror/Cucumber/Hooks/Hooks.java
@@ -0,0 +1,20 @@
+package org.FluffyTerror.Cucumber.Hooks;
+
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
+import org.FluffyTerror.managers.InitManager;
+
+public class Hooks {
+    @Before(order = 1)
+    public void setupDriver() {
+        InitManager.initFramework();
+    }
+
+    @AfterAll
+    public static void tearDownDriver() {
+        System.out.println("Закрываем веб-драйвер после всех тестов...");
+        InitManager.quitFramework();
+    }
+
+
+}
